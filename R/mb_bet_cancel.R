191e31350e54784ea9fe71ad9b464edb58769788
Niall Fitzgerald <nfitzgerald@xanadu.ie>
2015-09-25 17:28:46 +0100

    added mb_bet_cancel function

---
+++ R/mb_bet_cancel.R
@@ -0,0 +1,59 @@
+#' Perform a Bet Cancel Action
+#' @name Perform an Action to Cancel a Single or Multiple Exchange Bets
+#' @description This function provides bet cancellation functionality. Its possible to cancel a single bet by entering in a single value for the offer_id parameter. It also possible to cancel multiple bets at once by passing a vector of the offer_id parameter. Its also possible to cancel all bets from a given market, event or runner or vector of market, event or runner.
+#' @param session_data A session object returned from a successful mb_login attempt. It contains details about your user preferences and security details.
+#' @param offer_id The offer_id or vector of offer_ids that you want to cancel. 
+#' @param event_id The event_id or vector of event_ids that you want to cancel. 
+#' @param market_id The market_id or vector of market_ids that you want to cancel. 
+#' @param runner_id The runner_id or vector of runner_ids that you want to cancel. 
+#' @param cancel_all Boolean variable. Parameter to allow cancellation of all bets on all events/markets/runners. Default is FALSE.
+#' @return The status and details of your bet cancellations are returned.
+#' @seealso \code{\link{mb_get_bets},\link{mb_bet_place},\link{mb_bet_updatel}}
+#' @export 
+#' @examples
+#' \dontrun{my_session <- mb_login("my_user_name","verysafepassword"); 
+#' mb_bet_cancel(session_data=my_session,odds=2.5,stake=5,runner_id=12345)}
+#' 
+
+mb_bet_cancel <- function(session_data,offer_id=NULL,event_id=NULL,market_id=NULL,runner_id=NULL,cancel_all=FALSE)
+{ #mb_bet_place(session_data=session_details,runner_id=2265802,side='back',stake=2,odds=5)
+  ##session_data <- mb_login("xan_niallf","xan_n1allf");offer_id=NULL;event_id=NULL;market_id=NULL;runner_id=NULL;cancel_all=FALSE
+  content            <- NULL
+  if(is.null(session_data)){
+    print(paste("You have not provided data about your session in the session_data parameter. Please execute mb_login('my_user_name','verysafepassword') and save the resulting object in a variable e.g. my_session <- mb_login(username,pwd); and pass session_data=my_session as a parameter in this function."));return(content)
+  }
+  if(sum(offer_id%%1)>0) {print(paste("The offer_id values must be in integer format. Please amend and try again."));return(content)}
+  if(sum(event_id%%1)>0) {print(paste("The event_id values must be in integer format. Please amend and try again."));return(content)}
+  if(sum(market_id%%1)>0) {print(paste("The market_id values must be in integer format. Please amend and try again."));return(content)}
+  if(sum(runner_id%%1)>0) {print(paste("The runner_id values must be in integer format. Please amend and try again."));return(content)}
+  if(sum(!is.null(offer_id))==0&sum(!is.null(event_id))&sum(!is.null(market_id))&sum(!is.null(runner_id))&cancel_all==FALSE){
+    print(paste("No bets have been specified for cancellation, pelase try again."));return(content)
+  }
+  offer_action <- "";event_action <- "";market_action <- "";runner_action <- "";
+  if(sum(!is.null(offer_id))>0){
+    offer_action <- paste(',offer-ids'=paste(offer_id,collapse=","),sep="")
+  }
+  if(sum(!is.null(event_id))>0){
+    event_action <- paste(' event-ids'=paste(event_id,collapse=","),sep="")
+  }
+  if(sum(!is.null(market_id))>0){
+    market_action <- paste(' market-ids'=paste(market_id,collapse=","),sep="")
+  }
+  if(sum(!is.null(runner_id))>0){
+    runner_action <- paste(' runner-ids'=paste(runner_id,collapse=","),sep="")
+  }
+  body_data          <- paste("{'exchange-type':'back-lay','currency':'",session_data$currency,"','odds-type':'",session_data$odds_type,"' ",offer_action,event_action,market_action,runner_action,"}",sep="")
+  cancel_bet_resp    <- DELETE(paste("https://www.matchbook.com/bpapi/rest/offers",sep=""),body=body_data,set_cookies('session-token'=session_data$session_token),content_type_json(),accept_json(),add_headers('User-Agent'='rlibnf'))  
+  status_code        <- cancel_bet_resp$status_code  
+  if(status_code==200)
+  {
+    content <- fromJSON(content(cancel_bet_resp, "text", "application/json"))
+  } else
+  {
+    print(paste("Warning/Error in communicating with cancel bet at https://www.matchbook.com/bpapi/rest/offers",sep=""))
+  }
+  return(content)
+}
+
+
+
--- R/mb_bet_update.R
+++ R/mb_bet_update.R
@@ -1,17 +1,18 @@
 #' Perform a Bet Update Action
 #' @name Perform an Action to Update a Single or Multiple Exchange Bets
-#' @description This function provides bet update functionality. Its possible to update a simgle bet by entering in a single value for each of the runner_id, side, odds and stake parameters. It also possible to update multiple bets multiple bets at once by passing a vector for each of the runner_id, side, odds and stake parameters. In this instance, its imperative that the order in each of the vectors is preserved i.e. that the runner_id, side, odds and stake for the first update are the first elements of each parameter vector, and that the runner_id, side, odds and stake values for the second update are the second elements of each parameter vector and so on. There is a cap of 25 on the number of updates that can be placed at one time.
+#' @description This function provides bet update functionality. Its possible to update a single bet by entering in a single value for each of the offer_id, side, odds and stake parameters. It also possible to update multiple bets at once by passing a vector for each of the offer_id, side, odds and stake parameters. In this instance, its imperative that the order in each of the vectors is preserved i.e. that the runner_id, side, odds and stake for the first update are the first elements of each parameter vector, and that the offer_id, side, odds and stake values for the second update are the second elements of each parameter vector and so on. There is a cap of 25 on the number of updates that can be placed at one time.
 #' @param session_data A session object returned from a successful mb_login attempt. It contains details about your user preferences and security details.
 #' @param offer_id The id of the offer that you want to update. 
 #' @param side The side that you want place a bet. This has to be one of either 'back' or 'lay'.
 #' @param stake The amount that want to stake for this bet. The currency used is the currency you specified when you set up your Matchbook.com account. A real number format is required.
 #' @param odds The odds you want to place a bet at. The odds type is based on the information from your session_data, which is the default setting saved for your account.
-#' @return The status and details of your bet placement are returned.
-#' @seealso \code{\link{mb_get_bets}}
+#' @return The status and details of your bet updates are returned.
+#' @seealso \code{\link{mb_get_bets},\link{mb_bet_place},\link{mb_bet_cancel}}
 #' @export 
 #' @examples
 #' \dontrun{my_session <- mb_login("my_user_name","verysafepassword"); 
-#' mb_bet_place(session_data=my_session,odds=2.5,stake=5,runner_id=12345)}
+#' new_odds_value <- 20
+#' mb_bet_update(session_data=my_session,offer_id=12345,odds=new_odds_value)}
 #' 
 
 mb_bet_update <- function(session_data,offer_id,runner_id,side,stake,odds)
